e2ac0ddfafb874c4e25449c03874c1ae99212117
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2021-12-29 19:07:01 +0200

    Finished steps 10 and 11

--- part2.py
+++ part2.py
@@ -1,16 +1,15 @@
 # %%
 from collections import defaultdict
+from sys import implementation
+import joblib
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-import librosa
-# import pomegranate
-# from pomegranate.distributions import MultivariateGaussianDistribution
-# from pomegranate.gmm import GeneralMixtureModel
-# from pomegranate.hmm import HiddenMarkovModel
-from pomegranate import *
 from sklearn.model_selection import train_test_split
+
+import librosa
+from hmmlearn import hmm
 
 import torch
 import torch.nn as nn
@@ -34,7 +33,7 @@ DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
                                         test_size=0.8, stratify=y_train_all)
 
  
-# %% STEP 10
+# %% STEP 10, STEP 11
 def group_by_label(X, y):
     grouped = defaultdict(list)
     for a, b in zip(X, y):
@@ -42,52 +41,113 @@ def group_by_label(X, y):
     return grouped
 
 
+def reshape(G):
+    G_cat = np.concatenate(G, axis=0)
+    G_lengths = np.array(list(map(len, G)))
+    return G_cat, G_lengths
+
+
 grouped_train = group_by_label(X_train, y_train)
+grouped_val = group_by_label(X_val, y_val)
 grouped_test = group_by_label(X_test, y_test)
 classes = sorted(grouped_train.keys())
 
-n_components = 3 # 1..5
-n_states = 2  # 1..4
+##################################################################################
+# # The following doesn't work. Pomegranate calculates covariance matrices which
+# # are not positive definite for some reason.
+# # I will use hmmlearn instead.
 
-c = classes[0]  # TODO: variable in a for loop
-g_train = grouped_train[c]
+# from pomegranate.distributions import MultivariateGaussianDistribution
+# from pomegranate.gmm import GeneralMixtureModel
+# from pomegranate.hmm import HiddenMarkovModel
 
-#%%
-def create_and_fit_gmhmm(group):
+# def create_and_fit_gmmhmm(group, n_components=4, n_mix=5):
+
+#     group_cat = np.concatenate(group, axis=0, dtype=np.float64)
+#     distributions = []
+#     # Initialize the Gaussian Mixtures
+#     for _ in range(n_components):
+#         d = GeneralMixtureModel.from_samples(MultivariateGaussianDistribution,
+#                                             n_mix,
+#                                             group_cat)
+#         distributions.append(d)
+
+
+#     # Create a Left-Right uniform transition matrix
+#     transition_matrix = np.diag(np.ones(n_components))
+#     transition_matrix += np.diag(np.ones(n_components-1), 1)
+#     transition_matrix /= 2.
+#     transition_matrix[-1, -1] = 1.
+#     transition_matrix
+
+#     # Start at state 0 and end at state n_components-1
+#     starts = np.zeros(n_components)
+#     starts[0] = 1.
+#     ends = np.zeros(n_components)
+#     ends[-1] = 1.
+
+#     # Create the GMMHMM
+#     state_names = [f's{i}' for i in range(n_components)]
+#     model = HiddenMarkovModel.from_matrix(transition_matrix,
+#                                       distributions, starts, ends,
+#                                       state_names=state_names)
     
-    group_cat = np.concatenate(group, axis=0, dtype=np.float64)
-    distributions = []
-    # Initialize the Gaussian Mixtures
-    for _ in range(n_states):
-        d = GeneralMixtureModel.from_samples(MultivariateGaussianDistribution,
-                                            n_components,
-                                            group_cat)
-        distributions.append(d)
+#     # Fit and return the GMMHMM
+#     model.fit(group, max_iterations=5)
+#     return model
 
+    
+# models = {c: create_and_fit_gmmhmm(group) for c, group in grouped_train.items()}
+###################################################################################
+
+
+def create_gmmhmm(n_components=4, n_mix=5,
+                  covariance_type='full', algorithm='viterbi',
+                  tol=1e-2, n_iter=10, verbose=True):
 
     # Create a Left-Right uniform transition matrix
-    transition_matrix = np.triu(np.ones((n_states, n_states)))
-    transition_matrix /= np.arange(n_states, 0, -1)[..., np.newaxis]
+    transmat = np.diag(np.ones(n_components))
+    transmat += np.diag(np.ones(n_components-1), 1)
+    transmat /= np.sum(transmat, axis=1)[..., np.newaxis]
 
-    # Start at state 0 and end at state n_states-1
-    starts = np.zeros(n_states)
-    starts[0] = 1.
-    ends = np.zeros(n_states)
-    ends[-1] = 1.
+    # Start at state 0
+    startprob = np.zeros(n_components)
+    startprob[0] = 1.
+    # No need for an end_prob because n_components-1 is an (initially unique)
+    # absorbing state by the values of the initial transition matrix.
+    # The 0s of the transition matrix, stay at 0 (up to some numerical error),
+    # thus n_components-1 is still absorbing after training.
+    # Of course, there is a chance that n_components-1 stops being unique,
+    # but that chance is very slim.
+    # Also, there is no option for an endprob in hmmlearn.
 
-    # Create the GMHMM
-    state_names = [f's{i}' for i in range(n_states)]
-    model = HiddenMarkovModel.from_matrix(transition_matrix,
-                                      distributions, starts, ends,
-                                      state_names=state_names)
-    
-    # Fit and return the GMHMM
-    model.fit(group, max_iterations=5)
+    # Create the model.
+    # ‘s’ for startprob, ‘t’ for transmat, ‘m’ for means,
+    # ‘c’ for covars, and ‘w’ for GMM mixing weights
+    model = hmm.GMMHMM(n_components=n_components, n_mix=n_mix,
+                       covariance_type=covariance_type,
+                       n_iter=n_iter,
+                       tol=tol,  # loglikelihood increase
+                       init_params='mcw',
+                       params='tmcw',
+                       algorithm=algorithm,  # Decoder algorithm
+                       verbose=verbose)
+    model.startprob_ = startprob
+    model.transmat_ = transmat
+
     return model
 
-    
-# models = {c: create_and_fit_gmhmm(group) for c, group in grouped_train.items()}
-
-group = grouped_train[classes[0]]
-model = create_and_fit_gmhmm(group)
 # %%
+# Create and fit the models
+path = 'hmmgmm-models.joblib'
+# models = {}
+# for c in classes:
+#     print(f'TRAINING CLASS {c}')
+#     G_train, lens_train = reshape(grouped_train[c])
+#     model = create_gmmhmm(n_iter=1000)
+#     model.fit(G_train, lens_train)
+#     models[c] = model
+#     print('\n'*2)
+# joblib.dump(models, path)
+models = joblib.load(path)
+# %% STEP 12
